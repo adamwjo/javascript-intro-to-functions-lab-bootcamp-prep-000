5dba86c71315cfd37e544697932c4ba8badb01f4
Adam Johnson <adamwjo@gmail.com>
2018-06-19 19:09:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function logWhisper(hello) {
   console.log(hello.toLowerCase())
 }
 function sayHiToGrandma(hello) {
-  if (hello.toLowerCase() === hello) {
+  if ('hello.toLowerCase()' === hello) {
     return "I can\'t hear you!"
-  } if (hello.toUpperCase() === HELLO ) {
+  } if ('hello.toUpperCase()' === HELLO ) {
     return "YES INDEED!"
   } if (hello === "I love you, Grandma.") {
     return "I love you, too."
